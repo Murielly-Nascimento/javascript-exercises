8b213f485c8cf4292d89f87d5b873b7a25322abb
murielly <murielly525@gmail.com>
2024-09-29 17:13:19 -0300

    FIX: 04 solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let aux = [];
+
+    array.forEach((item) => {
+        if(!args.includes(item)){
+            aux.push(item);
+        }
+    })
+    return aux;
 };
 
 // Do not edit below this line
