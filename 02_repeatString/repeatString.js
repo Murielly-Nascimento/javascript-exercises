23e6f09198e558bdba2b423db894e8ad01f60c11
murielly <murielly525@gmail.com>
2024-09-29 16:47:10 -0300

    FIX: 02 solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, numberOfTimes) {
+    if(numberOfTimes < 0)
+        return "ERROR";
 
+    let aux = "";
+    for(let i = 0; i < numberOfTimes; i++){
+        aux += string;
+    }
+    return aux;
 };
 
 // Do not edit below this line
